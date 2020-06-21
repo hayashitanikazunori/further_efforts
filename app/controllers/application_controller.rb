1e5f397a57b6e1ba737af203c9cbf78e1d5bc680
hayashitani-kazunori <fworkkaz@gmail.com>
2020-06-22 00:59:34 +0900

    nameカラムストロングパラメーター設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  # ユーザー登録と編集フォームで受け取ったユーザー名をDBに保存するストロングパラメータ
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      devise_parameter_sanitizer.permit(:accoount_update, keys: [:name])
+    end
 end
