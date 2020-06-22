0760c102eaebb2def9eca746bc49ccb6f2bc1dfb
hayashitani-kazunori <fworkkaz@gmail.com>
2020-06-22 23:22:07 +0900

    rails g users_show

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
+  resources :users, only: [:show]
 
   root to: "tops#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
