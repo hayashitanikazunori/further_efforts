be60d282748ef4debc5bee60b86db5acd107149e
hayashitani-kazunori <fworkkaz@gmail.com>
2020-06-25 23:16:09 +0900

    posts_indexのフレームワークを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get 'posts/index'
   get 'posts/ranking'
-  get 'posts/show'
-  get 'posts/create'
-  get 'posts/edit'
   devise_for :users
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
   resources :users, only: [:show]
+  resources :posts
 
   root to: "tops#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
