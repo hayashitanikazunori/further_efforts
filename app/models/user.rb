86d9fd3ac7a390d2371f05c99a07d9202a77c416
hayashitani-kazunori <fworkkaz@gmail.com>
2020-07-09 17:15:11 +0900

    week_total_span error

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,16 +31,22 @@ class User < ApplicationRecord
     )
     return total.sum(:span)
   end
-  
-  # def learns
-  #   today = Date.current # 今日の日付を取得
-  #   @learns = User.find( 
-  #     Post.group(:user_id) # user_idでグループ化
-  #     .where(created_at: today.in_time_zone.all_week) # 今週の投稿のみで絞り込み
-  #     .order("sum(span) desc") # spanカラムを合計してそれを降順に並び替える
-  #     .pluck(:user_id) # 最後にuser_idを返す
-  #   )
-  # end
+
+  # kindカラムごとの今週の学習時間の合計
+  def week_total
+    today = Date.current
+    week_total_span = Post.group(:kind)
+      .where(created_at: today.in_time_zone.all_week, user_id: self.id)
+      .select('kind')
+  end
+
+  def week_total2
+    today = Date.current
+    week_total_span = Post.group(:kind)
+      .where(created_at: today.in_time_zone.all_week, user_id: self.id)
+      .select('span')
+      .sum(:span)
+  end
 
   # すでにいいねをしているかを調べる
   def already_liked?(post)
