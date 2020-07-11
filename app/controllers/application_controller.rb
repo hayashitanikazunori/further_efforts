504c81c1d2d08f658f9b1cd3a644e1333c07ab3f
hayashitani-kazunori <fworkkaz@gmail.com>
2020-07-11 19:24:08 +0900

    flash messages bootstrap適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(user)
+      posts_path
+  end
+  
+  def after_sign_out_path_for(user)
+    new_user_session_path
+  end
+
   protected
   # ユーザー登録と編集フォームで受け取ったユーザー名をDBに保存するストロングパラメータ
     def configure_permitted_parameters
