4a925c614200b9aa7d9b1c91ebb5982a8923cbdd
hayashitani-kazunori <fworkkaz@gmail.com>
2020-07-16 13:52:18 +0900

    userの学習日付を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,14 +32,16 @@ class User < ApplicationRecord
     return total.sum(:span)
   end
 
-  # kindカラムごとの今週の学習時間の合計
-  def week_total
+  def total_and_counts
     today = Date.current
-    week_total_span = Post.group(:kind)
-      .where(created_at: today.in_time_zone.all_week, user_id: self.id)
-      .sum(:span)
-  end
+    
+    total_spans = Post.group(:kind).where(user_id: self.id).sum(:span).to_a
+    week_total_spans = Post.group(:kind).where(created_at: today.in_time_zone.all_week, user_id: self.id).sum(:span)
+    total_counts = Post.group(:kind).where(user_id: self.id).count.values
 
+    total_spans.map.with_index {|total_span, i| total_span.push(week_total_spans.fetch(total_span[0], 0), total_counts[i])}
+  end
+ 
   # すでにいいねをしているかを調べる
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
