916f68a06031cc844d03dc6357685dc489364a41
hayashitani-kazunori <fworkkaz@gmail.com>
2020-07-26 15:43:05 +0900

    エラー解決

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -263,18 +263,14 @@ Devise.setup do |config|
 
   # Facebook.omniauth
   config.omniauth :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-    scope: 'email',
-    info_fields: 'email,name',
     callback_url: "#{ENV['HOST']}/users/auth/facebook/callback"
 
   # Twitter.omniauth
   config.omniauth :twitter, ENV['TWITTER_API_KEY'], ENV['TWITTER_API_SECRET'],
-    scope: 'email',
     oauth_callback: "#{ENV['HOST']}/users/auth/twitter/callback"
 
   # Google.omniauth
   config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_IDENV'], ENV['GOOGLE_CLIENT_SECRET'],
-    scope: 'email',
     redirect_uri: "#{ENV['HOST']}/users/auth/google_outh2/callback"
 
   # ==> Warden configuration
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'users/ranking'
 
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'user/omniauth_callbacks' }
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
