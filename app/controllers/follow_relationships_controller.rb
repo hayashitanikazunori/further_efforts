8e4cd8ee7d9a2e240f65ada8cecba9cc464a8f7d
hayashitani-kazunori <fworkkaz@gmail.com>
2020-08-02 13:55:47 +0900

    follow_relationships_controller refactored

--- app/controllers/follow_relationships_controller.rb
+++ app/controllers/follow_relationships_controller.rb
@@ -2,14 +2,12 @@ class FollowRelationshipsController < ApplicationController
   def create
     @user = User.find(params[:follow_relationship][:following_id])
     current_user.follow(@user)
-    flash[:notice] = "フォローしました。"
-    redirect_back(fallback_location: root_path)
+    redirect_to user_path(@user), notice: 'フォローしました'
   end
 
   def destroy
     @user = User.find(params[:follow_relationship][:following_id])
     current_user.unfollow(@user)
-    flash[:notice] = "フォローを解除しました。"
-    redirect_back(fallback_location: root_path)
+    redirect_to user_path(@user), notice: 'フォローを解除しました'
   end
 end
