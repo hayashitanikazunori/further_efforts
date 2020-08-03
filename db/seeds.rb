6e2206f59c5ee925d39b1c2afe1675e23734cf4f
hayashitani-kazunori <fworkkaz@gmail.com>
2020-08-04 00:48:59 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 後ほど対応
+# 10.times do |n|
+#   name = Faker::Games::Pokemon.name
+#   email = "tester#{n + 1}@test.com"
+#   password = "password"
+#   User.create!(
+#     name: name,
+#     email: email,
+#     password: password,
+#     password_confirmation: password,
+#   )
+# end
+
+# User.all.each do |user|
+#   user.posts.create!(
+#     kind: "JavaScript",
+#     body: "テスト",
+#     span: 100,
+#   )
+# end
